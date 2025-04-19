ca23ead3d2e1b80d0791a56da935dc0edb2383b7
Dzmitry Rybak <nakadisa@icloud.com>
2025-04-19 18:48:56 +0200

    graphql course starter

---
+++ schema.js
@@ -0,0 +1,26 @@
+export const typeDefs = `#graphql
+    type Game {
+        id: ID!, # not allow to be null
+        title: String!,
+        platform: [String!]!
+    }
+
+    type Review {
+        id: ID!
+        rating: Int!
+        content: String
+    }
+
+    type Author {
+        id: ID!
+        name: String!;
+        verified: Boolean!
+    }
+
+    # type Query - every graphql schema needs to have
+    type Query {
+        reviews: [Reviews]
+    }
+`
+
+// int, float, string, boolean, ID
